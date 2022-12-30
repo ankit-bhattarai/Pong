11d17751c757891613edbd907a2278ffdaff62e0
Ankit Bhattarai <ankitbhattarai.ab@protonmail.com>
2022-12-30 13:19:58 +0000

    Server.py update with Control and State objects and removed print statements

--- Server.py
+++ Server.py
@@ -1,25 +1,29 @@
 import socket
-from data import random_message, get_dictionary_from_stream
+from data import State, Control
 
 def connect_to_socket():
     print("Waiting to Connect")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('localhost', 55001))
-    print("Connected Succesfully")
     s.listen(10)
     conn, addr = s.accept()
+    print("Connected Succesfully")
     return conn, addr
 
 def main():
     conn, addr = connect_to_socket()
+    state = State()
+    control1 = Control(-1, state, 75, 490)
+    control2 = Control(1, state, 1425, 490)
+    state.get_control_objects(control1, control2)
     while True:
         data = conn.recv(1024)
         if not data:
             break
         try:
             decoded_data = data.decode()
-            print("Values: ", get_dictionary_from_stream(decoded_data))
-            message = random_message()
+            state.update_state(decoded_data)
+            message = state.output_message()
             output_message = str.encode(message)
         except UnicodeDecodeError:
             print("UnicodeDecodeError - can't decode, this is the raw data", data)
